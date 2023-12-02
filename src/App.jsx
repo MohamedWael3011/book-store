3c0fb66d0a96411b21ef6d58f81802fd7e3f2bc6
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2023-12-02 13:54:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="bg-green-500 w-full h-screen">
+    <h1 className="text-3xl font-bold underline">
+      Book Store!
+    </h1>
+    </div>
+  )
+}
+
+export default App
