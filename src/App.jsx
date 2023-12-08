9e8215e8fd0e868e46f2f3b3fee1d7310cd7d9de
Youssef ElAnsary <youssefmohamednbis@gmail.com>
2023-12-08 22:12:12 +0200

    fixes to edit page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import {Routes, Route} from 'react-router-dom'
 import BookTest from "./components/Home/BookTest.jsx"
 import AddGenre from "./components/AdminGenre_save/AddGenre.jsx"
 import Navbaradmin from "./components/NavAndFooter/NavAdmin.jsx"
+import Footer from "./components/NavAndFooter/Footer.jsx"
+import DeliveryForm from "./components/Delivery/DeliveryForm.jsx"
 import BookPage from "./components/Home/BookPage.jsx"
+
 function App() {
 
   return (
--- src/components/Home/BookPage.jsx
+++ src/components/Home/BookPage.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import Navbar from '../NavAndFooter/Navbar';
 import testbook from '../../components/assets/testbook.jpg';
 import './BookPage.css'
+import Footer from '../NavAndFooter/Footer';
 const BookPage = () => {
 return (
         <div>
@@ -23,33 +24,47 @@ return (
                 <div className='book_details_cont'>
                     <div className='book_details_item_cont'>
                         <span className='book_details_item_title'>ID:</span>
+                        <span className='input_book_details'></span>
                     </div>
                     <div className='book_details_item_cont'>
                         <span className='book_details_item_title'>Name:</span>
+                        <input type='text' className='input_book_details'></input>
                     </div>
                     <div className='book_details_item_cont'>
                         <span className='book_details_item_title'>Author:</span>
+                        <input type='text' className='input_book_details'></input>
                     </div>
                     <div className='book_details_item_cont'>
                         <span className='book_details_item_title'>Genre:</span>
+                        <input type='text' className='input_book_details'></input>
                     </div>
-                    <div className='book_details_item_cont'>
+                    <div className='book_desc_item_cont'>
                         <span className='book_details_item_title'>Description:</span>
+                        <textarea rows="4" cols="50" className='input_book_desc'>
+                        </textarea>
                     </div>
                     <div className='book_details_item_cont'>
                         <span className='book_details_item_title'>Price:</span>
+                        <input type='text' className='input_book_details'></input>
+                        <span className='units'>EGP</span>
                     </div>
                     <div className='book_details_item_cont'>
                         <span className='book_details_item_title'>Rates:</span>
+                        <input type='text' className='input_book_details'></input>
                     </div>
                     <div className='book_details_item_cont'>
                         <span className='book_details_item_title'>Number of sales:</span>
+                        <input type='text' className='input_book_details'></input>
+                        <span className='units'>Copy</span>
                     </div>
                     <div className='book_details_item_cont'>
                         <span className='book_details_item_title'>Quantity in Stock:</span>
+                        <input type='text' className='input_book_details'></input>
+                        <span className='units'>Copy</span>
                     </div>
                 </div>
             </div>
+            <Footer/>
         </div>
       )}
 
