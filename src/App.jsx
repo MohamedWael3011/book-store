4cb7a6a62f883f0054c039d318429981beda8011
fathy <2021170827@cis.asu.edu.eg>
2023-12-11 00:56:40 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,11 @@ import CategorizedBooks from "./components/CategorizedBooks/CategorizedBooks.jsx
 import Genrelist from "./components/CategorizedBooks/Genre.jsx"
 import EditBookPage from "./components/Home/EditBookPage.jsx"
 import BookPage from "./components/Home/BookPage.jsx"
-
 import Deliveryy from "./components/Delivery/DeliveryForm.jsx"
-<<<<<<< HEAD
 import Orders from "./components/UserOrders/Orders.jsx"
 import AdminOrders from "./components/AdminOrders/AdminOrders.jsx"
-=======
 import StarRatingAndReview from "./components/PropsAndComps/StarRatingAndReview.jsx"
- 
 import Homepage from"./components/Homepage/HomePage.jsx"
->>>>>>> b0b9ba66b2851761dfe70c2e7d778afb0310bdfa
 
 function App() {
 
@@ -31,12 +26,7 @@ function App() {
       <Route path="/register" element={<Signup/>}/>
     </Routes>
     <div>
-<<<<<<< HEAD
-        <AdminOrders/>
-=======
-        <Homepage/>
->>>>>>> b0b9ba66b2851761dfe70c2e7d778afb0310bdfa
-
+        <Orders/>
       </div></>
   )
 }
